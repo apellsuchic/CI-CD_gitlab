72b753557597b8085eb6cb581f0b18fa61bd9e18
Sasha Home <goodday.all@yandex.ru>
2025-04-16 14:19:52 +0300

    New

--- src/script.sh
+++ src/script.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-scp -r build/s21_cat adm-dep@10.1.30.37:/usr/local/bin
+scp -r src/build/s21_cat adm-dep@10.1.30.37:/usr/local/bin
 if [[ $? -eq 1 ]]; then 
     exit 1
 fi
-scp -r build/s21_grep adm-dep@10.1.30.37:/usr/local/bin
+scp -r src/build/s21_grep adm-dep@10.1.30.37:/usr/local/bin
 if [[ $? -eq 1 ]]; then 
     exit 1
 fi
