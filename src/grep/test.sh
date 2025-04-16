b90d1da27bd7bb66c3101215d1f073c9fa6a8060
Sasha Home <goodday.all@yandex.ru>
2025-04-16 09:07:49 +0300

    Update19

--- src/grep/test.sh
+++ src/grep/test.sh
@@ -306,11 +306,10 @@ result=$(diff a b)
 
 if [ $? == 0 ]; then
 	printf " TEST #$i ${GREEN}PASSED${NC}\n"
-	((failed++))
 else
 	printf " TEST #$i ${RED}FAILED${NC}\n"
 	printf "$result"
-	#((failed++))
+	((failed++))
 fi
 
 printf " ${GREEN}-----DONE[$((i - failed))/$((i))]-----${NC}\n"
