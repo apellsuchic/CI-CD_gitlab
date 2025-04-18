cb9d69e570cfb07fbdde6adc70bed8f12c9f64d9
Sasha Home <goodday.all@yandex.ru>
2025-04-18 12:55:19 +0300

    update2

--- src/bot.sh
+++ src/bot.sh
@@ -2,9 +2,9 @@
 
 BOT_TOKEN="7728592014:AAHsRXRhB0KHeVcq2H6lr_sv71V3KOyizBc"
 CHAT_ID="-1002606444402"
-MESSAGE="Deploy status: $CI_PIPELINE_STATUS%0A%0AProject:+
-$CI_PROJECT_NAME%0AURL:+$CI_PROJECT_URL/pipelines/
-$CI_PIPELINE_ID/%0ABranch:+$CI_COMMIT_REF_SLUG"
+MESSAGE="Deploy status: $CI_JOB_STATUS
+Project name: $CI_PROJECT_NAME
+ID pipeline: $CI_PIPELINE_ID"
 
 curl -X POST "https://api.telegram.org/bot${BOT_TOKEN}/sendMessage" \
      -H "Content-Type: application/json" \
